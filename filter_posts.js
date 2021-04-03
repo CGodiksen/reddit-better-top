77d435c1432bd90385e357b8cc5aee4c2e858389
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-04-04 01:46:02 +0200

    Added mutation observer that blocks half the posts randomly.

--- filter_posts.js
+++ filter_posts.js
@@ -1,1 +1,17 @@
-console.log("This is a Reddit page.");
+// Create an observer that filters a post every time it is added to the document.
+const observer = new MutationObserver((mutationList, _observer) => {
+  mutationList.forEach(mutation => {
+    if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+      mutation.addedNodes.forEach((node) => {
+        // If the new node is a post then check if it should be filtered.
+        if (node.childNodes.length > 0) {
+          if (node.id == "" && node.className == "" && Math.random() < 0.5) {
+            node.remove()
+          }
+        }
+      })
+    }
+  })
+});
+
+observer.observe(document, { childList: true, subtree: true });
