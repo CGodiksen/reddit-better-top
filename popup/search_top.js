d72e98c8ea406d8a5d68fb7709f4cdf6cef9672d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-04-03 18:06:19 +0200

    Initialize elements from the popup html file.

---
+++ popup/search_top.js
@@ -0,0 +1,5 @@
+// Initialize elements from the popup html file.
+const timeLimitNumberInput = document.querySelector("div.popup-content input[name='time_limit_number']")
+const timeLimitUnitInput = document.querySelector("div.popup-content select[name='time_limit_unit']")
+
+const searchTopBtn = document.querySelector("div.popup-content button[name='search_top']")
