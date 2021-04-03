c3265423c4c929d06a10adaa8fad4af8ac9bed25
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-04-03 21:28:38 +0200

    Added function to change number input max.

--- popup/search_top.js
+++ popup/search_top.js
@@ -1,5 +1,18 @@
 // Initialize elements from the popup html file.
 const timeLimitNumberInput = document.querySelector("div.popup-content input[name='time_limit_number']")
-const timeLimitUnitInput = document.querySelector("div.popup-content select[name='time_limit_unit']")
+const timeLimitUnitSelect = document.querySelector("div.popup-content select[name='time_limit_unit']")
 
 const searchTopBtn = document.querySelector("div.popup-content button[name='search_top']")
+
+const changeInputMax = () => {
+  switch (timeLimitUnitSelect.value) {
+    case "Days":
+      timeLimitNumberInput.setAttribute("max", "31")
+      break;
+    case "Months":
+      timeLimitNumberInput.setAttribute("max", "12")
+      break;
+    case "Years":
+      timeLimitNumberInput.setAttribute("max", "99")
+  }
+}
