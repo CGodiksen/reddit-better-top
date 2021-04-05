bfadae30a21a264a5041ab9ae6f19556b5fe257b
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-04-05 23:33:42 +0200

    Implemented options injected into top dropdown.

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 browser.runtime.onMessage.addListener((request, sender) => {
-  // Listen for messages from content script, requesting that the post filter is started after the sender tab is loaded.
+  // Listen for messages from content script, requesting that the post filter is started after the sender tab is done loading.
   if (request.startFilterOnUpdate) {
     filter = { tabId: sender.tab.id }
-    browser.tabs.onUpdated.addListener((_tabId, _changeInfo, tabInfo) => requestFilterStart(tabInfo, "hello"), filter);
+    browser.tabs.onUpdated.addListener((tabId, _changeInfo, tabInfo) => requestFilterStart(tabId, tabInfo, request.timeLimitNumber, request.timeLimitUnit), filter);
   }
 })
 
-const requestFilterStart = (tabInfo, test) => {
-  console.log(tabInfo, test);
+const requestFilterStart = (tabId, tabInfo, timeLimitNumber, timeLimitUnit) => {
+  if (tabInfo.status == "complete") {
+    browser.tabs.sendMessage(tabId, { startFilter: true, timeLimitNumber: timeLimitNumber, timeLimitUnit: timeLimitUnit })
+  }
 }
