1d878d50ba0a6bfbf2284f7a66a3e07f2ea04f81
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-04-05 23:26:47 +0200

    Configured event listener and callback structure.

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 browser.runtime.onMessage.addListener((request, sender) => {
   // Listen for messages from content script, requesting that the post filter is started after the sender tab is loaded.
   if (request.startFilterOnUpdate) {
-    console.log(request, sender.tab.id);
+    filter = { tabId: sender.tab.id }
+    browser.tabs.onUpdated.addListener((_tabId, _changeInfo, tabInfo) => requestFilterStart(tabInfo, "hello"), filter);
   }
 })
+
+const requestFilterStart = (tabInfo, test) => {
+  console.log(tabInfo, test);
+}
